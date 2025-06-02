e0f5976ff902a52c859cc3129204c9d3257b085e
melissa <melissa.chang@aiwin.com.tw>
2025-06-02 15:00:35 +0800

    eslint setting changes

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import sortKeysFix from 'eslint-plugin-sort-keys-fix'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,6 +18,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
+      'sort-keys-fix': sortKeysFix,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +27,9 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'sort-keys-fix/sort-keys-fix': 'warn',
     },
   },
 )
